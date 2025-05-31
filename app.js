2fede706c940daaed82829aafdbcaa269655efc2
Lathifah Zumaemah <lathifahzumaemah@gmail.com>
2025-05-31 15:02:45 +0700

    Update CI config and app.js for better monitoring

--- app.js
+++ app.js
@@ -6,16 +6,23 @@ const appInsights = require('applicationinsights');
 const app = express();
 const PORT = 3000;
 
-// Setup Application Insights
-appInsights.setup("<98270dfe-4fc5-4639-9f6d-ab9fdcf83809>")
-  .setAutoCollectRequests(true)         // track HTTP request otomatis
-  .setAutoCollectPerformance(true)      // track performa app (CPU, memori)
-  .setAutoCollectExceptions(true)       // track error
-  .setAutoCollectDependencies(true)     // track dependency (DB, API)
-  .setAutoDependencyCorrelation(true)
-  .start();
+const instrKey = process.env.APPINSIGHTS_INSTRUMENTATIONKEY || process.env.APPLICATIONINSIGHTS_CONNECTION_STRING;
 
-const client = appInsights.defaultClient;
+// Setup Application Insights
+if (instrKey) {
+  appInsights.setup(instrKey)
+    .setAutoCollectRequests(true)         // track HTTP request otomatis
+    .setAutoCollectPerformance(true)      // track performa app (CPU, memori)
+    .setAutoCollectExceptions(true)       // track error
+    .setAutoCollectDependencies(true)     // track dependency (DB, API)
+    .setAutoDependencyCorrelation(true)
+    .start();
+
+  const client = appInsights.defaultClient;
+  client.trackEvent({name: "app_started"});
+} else {
+  console.warn("Application Insights Instrumentation Key tidak ditemukan. Telemetry tidak diaktifkan.");
+}
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
