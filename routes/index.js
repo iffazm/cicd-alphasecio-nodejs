3555593d6ccd07b768ea19c6892e6d4ef319a00f
Lathifah Zumaemah <lathifahzumaemah@gmail.com>
2025-06-28 12:35:33 +0700

    Feature update: Frontend for Auth Feature

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,8 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
-// Serve the index.html file for the root route
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../views/index.html'));
+router.get('/app', (req, res) => {
+  res.sendFile(path.join(__dirname, '../views/app.html'));
 });
 
 module.exports = router;
